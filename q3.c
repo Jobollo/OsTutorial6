834dbf3c05d0f6ad1d85e11c99329cfcb2170415
Robert Amenta <robert.amenta@uoit.net>
2018-03-04 21:03:03 -0500

    maybe fixed

--- q3.c
+++ q3.c
@@ -10,31 +10,32 @@ sem_t mutex;
 int moving_sum[5] = {0,0,0,0,0};
 
 typedef struct {
-	int number[5];
+	int number;
 } num;
 num nums[5];
-void* factorial(void *arg)
+void* factorial(int i)
 {
 	pthread_t id = pthread_self();
-	int c, n, i=0, fact = 1;
+	int c, n, fact = 1;
 	n=nums[i].number;
-	printf("%d",n);
+	//printf("%d",n);
 	for (c=1;c<=n;c++)
 		fact = fact*c;
 	printf("factorial of %d = %d\n", n, fact);
-	
-	while (moving_sum[i-1] <= 0)
+	if (i==0)
 	{
-		sem_wait(&mutex);
-		if (moving_sum[i-1] > 0)
-		{
-			fact += moving_sum[i-1];
-			moving_sum[i] += fact;
-		}
-		sem_post(&mutex);
-		i++;
+		moving_sum[i] = fact;
 	}
-	
+	else
+	{
+			sem_wait(&mutex);
+			if (moving_sum[i-1] > 0)
+			{
+				fact += moving_sum[i-1];
+				moving_sum[i] += fact;
+			}
+			sem_post(&mutex);
+	}
 	return NULL;
 }
 
@@ -48,15 +49,31 @@ int main(void)
 	for (i=0; i<5; i++)
 	{
 		printf("Enter number %d: ", i+1);
-		scanf("%d",&nums.number[i]);
-		printf("%d",nums.number[i]);
-		err = pthread_create(&(tid[i]),NULL,&factorial,NULL);
+		scanf("%d",&x);
+		nums[i].number = x;
+		//printf("%d",nums[i].number);
+		err = pthread_create(&(tid[i]),NULL,&factorial,i);
 		if (err != 0)
 			printf("\nCan't create thread: [%s]", strerror(err));
 		else
 			printf("\n Thread created\n");
+		sleep(1);
 	}
+	for (i=0; i<5; i++)
+	{
+		if(pthread_join(tid[i],NULL))
+		{
+			printf("Could not join thread %d\n", i);
+			return -1;
+		}
+	}	
+	sem_destroy(&mutex);
 
+	for (i=0; i<5; i++)
+	{
+		printf("%d ", moving_sum[i]);
+	}	
+	printf("\n");
 	return 0;
 	
 
